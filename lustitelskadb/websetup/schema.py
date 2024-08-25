eb6685833459ba6d4c3102db024667dab5d6c346
Jaroslav Mikulík <byczech@malyjarda.cz>
2024-08-25 09:13:36 +0200

    Added pluggables alembic migration on setup-app to be on head

--- lustitelskadb/websetup/schema.py
+++ lustitelskadb/websetup/schema.py
@@ -3,7 +3,9 @@
 from __future__ import print_function
 
 from tg import config
+from tgext.pluggable import plugged
 import transaction
+import alembic.config
 
 
 def setup_schema(command, conf, vars):
@@ -19,10 +21,24 @@ def setup_schema(command, conf, vars):
     model.metadata.create_all(bind=config['tg.app_globals'].sa_engine)
     # <websetup.websetup.schema.after.metadata.create_all>
     transaction.commit()
+
     print('Initializing Migrations')
-    import alembic.config
     alembic_cfg = alembic.config.Config()
     alembic_cfg.set_main_option("script_location", "migration")
     alembic_cfg.set_main_option("sqlalchemy.url", config['sqlalchemy.url'])
     import alembic.command
     alembic.command.stamp(alembic_cfg, "head")
+
+    for plugapp in plugged():
+        try:
+            __import__("{}.migration".format(plugapp))
+            print('Setting migration stamp for', plugapp)
+            alembic_cfg.set_main_option("script_location", "{}:migration".format(plugapp))
+            alembic.command.stamp(alembic_cfg, "head")
+        except (ImportError, ModuleNotFoundError, alembic.util.exc.CommandError):
+            # quietly pass if plugapp hasn't migration enabled
+            # print('No migration enabled')
+            pass
+    # <websetup.websetup.schema.after.alembic.command.stamp>
+
+    transaction.commit()
