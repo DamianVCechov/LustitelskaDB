ea7956bfbf1f11ca65b36caf949a5624501fd481
tlucekP <tluchor.peter@outlook.cz>
2024-09-11 18:06:33 +0200

    část kodu pro počítání bodů a updatování.

--- lustitelskadb/controllers/admin.py
+++ lustitelskadb/controllers/admin.py
@@ -2,6 +2,7 @@
 """Administration controller module."""
 
 import logging
+from dns.rdataclass import NONE
 log = logging.getLogger(__name__)
 
 from tg import expose, redirect, validate, flash, url, require, tmpl_context, config, request
@@ -30,6 +31,10 @@ import csv
 import requests
 
 import tw2.core as twc
+
+#from sqlalchemy.orm import session
+#from sqlalchemy import func
+#from models import GameResult
 
 __all__ = ['AdministrationController']
 
@@ -117,64 +122,117 @@ class AdministrationController(BaseController):
             }
             parsed_vals = {}
 
-            try:
-                for row in reader:
-                    statistics['all_count'] += 1
-                    for key in parsed_vals:
-                        parsed_vals[key] = None
+            # try:
+            for row in reader:
+                statistics['all_count'] += 1
+                for key in parsed_vals:
+                    parsed_vals[key] = None
 
-                    sp_result = row[config.get('legacyimport.colname.result')].split()
-                    for s in sp_result:
-                        if s.find('#den') != -1:
-                            parsed_vals['game_no'] = int(s.lstrip('#den'))
-                        elif s.find('#krok') != -1:
-                            parsed_vals['step'] = int(s.lstrip('#krok'))
-                    if sp_result[-2].endswith('min') and sp_result[-1].endswith('s'):
-                        parsed_vals['time'] = int(sp_result[-2].rstrip('min')) * 60 + int(sp_result[-1].rstrip('s'))
+                sp_result = row[config.get('legacyimport.colname.result')].split()
+                for s in sp_result:
+                    if s.find('#den') != -1:
+                        parsed_vals['game_no'] = int(s.lstrip('#den'))
+                    elif s.find('#krok') != -1:
+                        parsed_vals['step'] = int(s.lstrip('#krok'))
+                if sp_result[-2].endswith('min') and sp_result[-1].endswith('s'):
+                    parsed_vals['time'] = int(sp_result[-2].rstrip('min')) * 60 + int(sp_result[-1].rstrip('s'))
 
-                    xuser = DBSession.query(model.XTwitter).filter(model.XTwitter.user_name == row[config.get('legacyimport.colname.nickname')]).first()
-                    if not xuser:
-                        xuser = model.XTwitter(
-                            xid=row[config.get('legacyimport.colname.nickname')],
-                            user_name=row[config.get('legacyimport.colname.nickname')],
-                            display_name=row[config.get('legacyimport.colname.nickname')]
-                        )
-                        DBSession.add(xuser)
-                        DBSession.flush()
-                        DBSession.refresh(xuser)
-                        statistics['added_nicknames'] += 1
+                xuser = DBSession.query(model.XTwitter).filter(model.XTwitter.user_name == row[config.get('legacyimport.colname.nickname')]).first()
+                if not xuser:
+                    xuser = model.XTwitter(
+                        xid=row[config.get('legacyimport.colname.nickname')],
+                        user_name=row[config.get('legacyimport.colname.nickname')],
+                        display_name=row[config.get('legacyimport.colname.nickname')]
+                    )
+                    DBSession.add(xuser)
+                    DBSession.flush()
+                    DBSession.refresh(xuser)
+                    statistics['added_nicknames'] += 1
 
-                    game = DBSession.query(model.GameResult, model.XTwitter).join(model.XTwitter, model.XTwitter.uid == model.GameResult.xtwitter_uid)
-                    game = game.filter(
-                        model.GameResult.game_no == parsed_vals['game_no'],
-                        model.GameResult.game_time == parsed_vals['time'],
-                        model.GameResult.game_rows == parsed_vals['step'],
-                        model.GameResult.comment == row[config.get('legacyimport.colname.comment')] if row[config.get('legacyimport.colname.comment')] else None,
-                        model.GameResult.created == datetime.strptime(row[config.get('legacyimport.colname.created')], "%d.%m.%Y %H:%M:%S")
-                    ).first()
+                game = DBSession.query(model.GameResult, model.XTwitter).join(model.XTwitter, model.XTwitter.uid == model.GameResult.xtwitter_uid)
+                game = game.filter(
+                    model.GameResult.game_no == parsed_vals['game_no'],
+                    model.GameResult.game_time == parsed_vals['time'],
+                    model.GameResult.game_rows == parsed_vals['step'],
+                    model.GameResult.comment == row[config.get('legacyimport.colname.comment')] if row[config.get('legacyimport.colname.comment')] else None,
+                    model.GameResult.created == datetime.strptime(row[config.get('legacyimport.colname.created')], "%d.%m.%Y %H:%M:%S")
+                ).first()
 
-                    if game:
-                        statistics['duplicity_skipped'] += 1
-                    else:
-                        game = model.GameResult(
-                            game_no=parsed_vals['game_no'],
-                            game_time=timedelta(seconds=parsed_vals['time']) if parsed_vals['time'] and parsed_vals['step'] else None,
-                            game_rows=parsed_vals['step'],
-                            wednesday_challenge={'': None, '✅': True, '❎': False}[row[config.get('legacyimport.colname.wednesdaychallenge')]],
-                            comment=row[config.get('legacyimport.colname.comment')],
-                            game_result_time=timedelta(seconds=parsed_vals['time'] + (parsed_vals['step'] - 1) * 12) if parsed_vals['time'] and parsed_vals['step'] else None,
-                            game_raw_data=row[config.get('legacyimport.colname.result')],
-                            created=datetime.strptime(row[config.get('legacyimport.colname.created')], "%d.%m.%Y %H:%M:%S"),
-                            xtwitter=xuser
-                        )
-                        DBSession.add(game)
-                        DBSession.flush()
-                        statistics['imported_count'] += 1
-                flash(_(u"The import was successful. {all_count} records processed, {imported_count} records imported, {duplicity_skipped} duplicate records skipped, and {added_nicknames} users added.").format(**statistics))
-            except Exception as e:
-                flash(_(u'The import attempt ended with an error: "{}"').format(e))
+                if game:
+                    statistics['duplicity_skipped'] += 1
+                else:
+                    game = model.GameResult(
+                        game_no=parsed_vals['game_no'],
+                        game_time=timedelta(seconds=parsed_vals['time']) if parsed_vals['time'] and parsed_vals['step'] else None,
+                        game_rows=parsed_vals['step'],
+                        wednesday_challenge={'': None, '✅': True, '❎': False}[row[config.get('legacyimport.colname.wednesdaychallenge')]],
+                        comment=row[config.get('legacyimport.colname.comment')],
+                        game_result_time=timedelta(seconds=parsed_vals['time'] + (parsed_vals['step'] - 1) * 12) if parsed_vals['time'] and parsed_vals['step'] else None,
+                        game_raw_data=row[config.get('legacyimport.colname.result')],
+                        created=datetime.strptime(row[config.get('legacyimport.colname.created')], "%d.%m.%Y %H:%M:%S"),
+                        xtwitter=xuser
+                    )
+                    DBSession.add(game)
+                    DBSession.flush()
+                    statistics['imported_count'] += 1
+            flash(_(u"The import was successful. {all_count} records processed, {imported_count} records imported, {duplicity_skipped} duplicate records skipped, and {added_nicknames} users added.").format(**statistics))
+            # except Exception as e:
+            #     flash(_(u'The import attempt ended with an error: "{}"').format(e))
 
         redirect('/admin')
+
+    # Moje část kodu
+    @expose('lustitelskadb.templates.administration.recountresults')
+    def recount_results(self):
+        """Recount game results."""
+        return dict(page='administration/recount_results')
+
+    #def calculate_game_points(attempts: int) -> int:
+    #    """Updates points for a given game. Assigns points to players according to their order."""
+    #    if rank == 1:
+    #        return 6
+    #    elif rank == 2:
+    #        return 5
+    #    elif rank == 3:
+    #        return 4
+    #    elif rank == 4:
+    #        return 3
+    #    elif rank == 5:
+    #        return 2
+    #    else return 1
+    #
+    #def update_player_ranking(session: Session):
+    #    """Updates the order of players and assigns points according to their results."""
+    #players = session.querry(GameResult.xtwitter_uid, GameResult.game_result_time, GameResult.game_rows)\
+    #                .filter(GameResult.game_result_time.isnot(None))\
+    #                .order_by(GameResult.game_result_time, GameResult.game_rows)\
+    #                .all()
+    #
+    #Unicorn_player = None
+    #
+    #for rank, player in enumerate(players, start=1):
+    #    if player.game_rows == 1:
+    #        Unicorn_player = player
+    #    else:
+    #        points = assign_points_based_on_rank(rank)
+    #        session.query(GameResult)\
+    #                .filter_by(xtwitter_uid=player.xtwitter_uid)\
+    #                .update({'game_points': points, 'game_rank': rank})
+    #
+    #if Unicorn_player:
+    #    session.querry(GameResult)\
+    #            .filter_by(xtwitter_uid=unicorn_player.xtwitter_uid)\
+    #            .update({'game_points': 0, 'game_rank': '🦄'})
+    #
+    #session.querry(GameResult)\
+    #        .filter(GameResult.game_result_time.is_(None))\
+    #        .update({'game_points': 0, 'game_rank': None})
+    #
+    #session.commit()
+    #
+    #def process_game_results(session: Session, game_no: int, attempts: int):
+    #    update_player_ranking(session)
+
 
     @expose('lustitelskadb.templates.administration.restart')
     @require(has_any_permission('manage', 'restartapp', msg=l_('Only for users with appropriate permissions')))
