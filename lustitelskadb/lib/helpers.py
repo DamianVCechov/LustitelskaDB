7909b360bc84fd71f3f23c859eabc68cbde90b9e
Jaroslav Mikulík <byczech@malyjarda.cz>
2025-01-20 02:08:01 +0100

    Improved helpers
    * moved wednesday_challenge_comming from one template to helpers
    
    * added now() and today() helpers
    
    * moved operators used in some templates to helpers

--- lustitelskadb/lib/helpers.py
+++ lustitelskadb/lib/helpers.py
@@ -2,9 +2,17 @@
 """Template Helpers used in LustitelskaDB."""
 import logging
 from markupsafe import Markup
-from datetime import datetime
+from datetime import datetime, date
 
 log = logging.getLogger(__name__)
+
+
+def now():
+    return datetime.now()
+
+
+def today():
+    return date.today()
 
 
 def current_year():
@@ -19,6 +27,17 @@ def icon(icon_name):
 def bicon(icon_name, fs=None, color=None):
     return Markup('<i class="bi bi-%s%s%s"></i>' % (icon_name, (' fs-%i' % fs) if fs else '', (' text-%s' % color) if color else ''))
 
+
+def wednesday_challenge_comming():
+    now = datetime.now()
+    if now.weekday() == 2 and now.hour >= 18 or now.weekday() == 3 and now.hour < 18:
+        return True
+    else:
+        return False
+
+
+# Import some operators for using in templates, because of problematic using > and < chars
+from operator import lt, le, gt, ge
 
 # Import commonly used helpers from WebHelpers2 and TG
 from tg.util.html import script_json_encode
