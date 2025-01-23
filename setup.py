9fabc5fd450dbbf67fe14f67f830897c66e4dce5
Jaroslav Mikulík <byczech@malyjarda.cz>
2025-01-23 01:51:28 +0100

    Version set to 0.46

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if py_version != (3, 2):
 
 setup(
     name='LustitelskaDB',
-    version='0.45',
+    version='0.46',
     description='Hobby web application for statistics czech Wordle game named HadejSlova',
     author='ByCzech, DamianVCechov',
     author_email='byczech@gmail.com, lustitele@gmail.com',
