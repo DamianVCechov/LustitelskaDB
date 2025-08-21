a2a753b98f4f6737d31954d12f3d58fd282739f1
Jaroslav Mikulík <byczech@malyjarda.cz>
2025-08-21 04:59:11 +0200

    Game result isn't public for on going game now

--- lustitelskadb/controllers/root.py
+++ lustitelskadb/controllers/root.py
@@ -237,7 +237,7 @@ class RootController(BaseController):
         solved_games = DBSession.query(model.GameResult).filter(model.GameResult.user_id == gameresult.user_id, model.GameResult.game_no <= gameresult.game_no, model.GameResult.game_rows != None).count()
         obtained_lanterns = DBSession.query(model.GameResult).filter(model.GameResult.user_id == gameresult.user_id, model.GameResult.game_no <= gameresult.game_no, model.GameResult.game_rows > 1, model.GameResult.game_time != None, model.GameResult.game_points == 0).count()
 
-        return dict(page="detail", gameresult=gameresult, user_game_stats=user_game_stats, played_games=played_games, solved_games=solved_games, obtained_lanterns=obtained_lanterns, user_game_rank_stats=user_game_rank_stats)
+        return dict(page="detail", gameresult=gameresult, user_game_stats=user_game_stats, played_games=played_games, solved_games=solved_games, obtained_lanterns=obtained_lanterns, user_game_rank_stats=user_game_rank_stats, game_in_progress=today_game_no())
 
     @expose()
     def xauthorize(self, **kw):
