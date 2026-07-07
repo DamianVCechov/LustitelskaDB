2944e760a0b57fb95b844bc58fb18c6d39885383
Jaroslav Mikulík <byczech@malyjarda.cz>
2026-07-08 00:26:54 +0200

    fix: added monkeypatch for Sprox + SQLAlchemy 2.0+

--- lustitelskadb/config/app_cfg.py
+++ lustitelskadb/config/app_cfg.py
@@ -59,6 +59,29 @@ base_config.update_blueprint({
     'model': lustitelskadb.model,
     'DBSession': lustitelskadb.model.DBSession,
 })
+
+import sqlalchemy
+
+# Monkeypatching for Sprox + SQLAlchemy 2.0+
+if sqlalchemy.__version__.startswith('2.'):
+    from sqlalchemy import Table, MetaData
+    from sqlalchemy.orm import DeclarativeMeta
+
+    if not hasattr(Table, 'bind'):
+        @property
+        def table_bind_compat(self):
+            return getattr(self.metadata, 'bind', None)
+        Table.bind = table_bind_compat
+
+    if not hasattr(MetaData, 'bind'):
+        @property
+        def metadata_bind_compat(self):
+            return getattr(self, '_bind', None)
+        @metadata_bind_compat.setter
+        def metadata_bind_compat(self, value):
+            self._bind = value
+        MetaData.bind = metadata_bind_compat
+
 # This tells to TurboGears how to retrieve the data for your user
 from tg.configuration.auth import TGAuthMetadata
 
