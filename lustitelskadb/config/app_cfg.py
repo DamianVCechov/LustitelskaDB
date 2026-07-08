17505c4f96b7596423f80803c5b4f9a969cb3006
Jaroslav Mikulík <byczech@malyjarda.cz>
2026-07-08 16:58:55 +0200

    feat: monkeypatches moved to their own module

--- lustitelskadb/config/app_cfg.py
+++ lustitelskadb/config/app_cfg.py
@@ -60,27 +60,11 @@ base_config.update_blueprint({
     'DBSession': lustitelskadb.model.DBSession,
 })
 
-import sqlalchemy
+from lustitelskadb.lib.monkeypatch import *
 
 # Monkeypatching for Sprox + SQLAlchemy 2.0+
-if sqlalchemy.__version__.startswith('2.'):
-    from sqlalchemy import Table, MetaData
-    from sqlalchemy.orm import DeclarativeMeta
+patch_sprox_sqla20()
 
-    if not hasattr(Table, 'bind'):
-        @property
-        def table_bind_compat(self):
-            return getattr(self.metadata, 'bind', None)
-        Table.bind = table_bind_compat
-
-    if not hasattr(MetaData, 'bind'):
-        @property
-        def metadata_bind_compat(self):
-            return getattr(self, '_bind', None)
-        @metadata_bind_compat.setter
-        def metadata_bind_compat(self, value):
-            self._bind = value
-        MetaData.bind = metadata_bind_compat
 
 # This tells to TurboGears how to retrieve the data for your user
 from tg.configuration.auth import TGAuthMetadata
---
+++ lustitelskadb/lib/monkeypatch/__init__.py
@@ -0,0 +1,3 @@
+from lustitelskadb.lib.monkeypatch.patch_sprox_sqla20 import patch_sprox_sqla20
+
+__all__ = ('patch_sprox_sqla20',)
